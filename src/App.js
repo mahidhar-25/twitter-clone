ea872096f639bc0217c9eb837eb72fcb6020a23a
mahidhar reddy <mahidhar100008@gmail.com>
2022-12-08 21:20:22 +0530

    added material ui package

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>hi</h1>
+       {/* sider bar */}
+       {/* feed */}
+       {/* widgets */}
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../../css/Sidebar.css'
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/*twitter icon */}
+
+      {/*sidebar option */}
+      {/*sidebar option */}
+      {/*sidebar option */}
+      {/*sidebar option */}
+      {/*sidebar option */}
+      {/*sidebar option */}
+
+      {/*button->tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
